a5e3ac6ae89ecae27430422449c516fa53e3d6fa
jgallern <121575424+jgallern@users.noreply.github.com>
2025-05-14 21:51:00 +0200

    patch d'un break

--- EasySaveV1/src/View/ManageBackUpView.cs
+++ EasySaveV1/src/View/ManageBackUpView.cs
@@ -199,6 +199,7 @@ namespace BackUp.View
                                         break;
                                 }
                             }while (key != ConsoleKey.Escape);
+                            break;
                         }
                         else
                         {
