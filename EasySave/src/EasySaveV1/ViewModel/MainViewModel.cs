652c8582c4bad38b18fdcb22cd0f2f2a56a34045
Redyfly <hirlibaptiste@gmail.com>
2025-05-23 16:24:46 +0200

    Interface avec datagrid + ajout de MainViewModel.cs

---
+++ EasySave/src/EasySaveV1/ViewModel/MainViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        // Événement obligatoire pour le binding
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // Méthode utilitaire pour déclencher l'événement
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        // Exemple de propriété liée à l'interface
+        private string _titre;
+        public string Titre
+        {
+            get => _titre;
+            set
+            {
+                if (_titre != value)
+                {
+                    _titre = value;
+                    OnPropertyChanged(); // Notifie la vue
+                }
+            }
+        }
+
+        // Constructeur
+        public MainViewModel()
+        {
+            Titre = "Bienvenue dans EasySave !";
+        }
+    }
+}
