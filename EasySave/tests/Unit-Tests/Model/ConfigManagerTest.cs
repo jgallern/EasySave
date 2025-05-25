f7397ca3ef5eac95635a3f3999923a7a0bfa4efb
FlorianLantz <florian.lantz@viacesi.fr>
2025-05-25 13:12:01 +0200

    fix path for linux machiine git

--- EasySave/tests/Unit-Tests/Model/ConfigManagerTest.cs
+++ EasySave/tests/Unit-Tests/Model/ConfigManagerTest.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Core.Model;
+using System.IO;
 
 namespace Unit_Tests
 {
@@ -12,9 +13,24 @@ namespace Unit_Tests
         [Fact]
         public void Main()
         {
-            BackUpJob bcktest = new BackUpJob("test", "c:/users/jg/test1", "c:/users/jg/test", false, false);    
-            bcktest.CreateJob();    
-            BackUpJob bcktest1 = new BackUpJob("test1", "c:/users/jg/test1", "c:/users/jg/test1", false, false);    
+
+            string basePath = Path.Combine(Path.GetTempPath(), "easysave-test");
+            string sourcePath1 = Path.Combine(basePath, "testsource1");
+            string destPath1 = Path.Combine(basePath, "testdest1");
+
+            Directory.CreateDirectory(sourcePath1);
+            Directory.CreateDirectory(destPath1);
+
+            BackUpJob bcktest = new BackUpJob("test1", sourcePath1, destPath1, false, false);    
+            bcktest.CreateJob();
+
+            string sourcePath2 = Path.Combine(basePath, "testsource2");
+            string destPath2 = Path.Combine(basePath, "testdest2");
+
+            Directory.CreateDirectory(sourcePath2);
+            Directory.CreateDirectory(destPath2);
+
+            BackUpJob bcktest1 = new BackUpJob("test2", sourcePath2, destPath2, false, false);    
             bcktest1.CreateJob();    
             bcktest.DeleteJob();
         }
