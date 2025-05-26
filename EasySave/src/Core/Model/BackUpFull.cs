2f11bc696a335e96fe603f37e020edc9dedaaedd
jgallern <121575424+jgallern@users.noreply.github.com>
2025-05-26 10:54:51 +0200

    encryption pour backupfull fonctionnelle

--- EasySave/src/Core/Model/BackUpFull.cs
+++ EasySave/src/Core/Model/BackUpFull.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics;
+using System.Security.Cryptography;
 using System.IO;
+using Core.Model.Managers;
 
 namespace Core.Model
 {
@@ -21,8 +23,9 @@ namespace Core.Model
 		{
             Stopwatch jobTimer = Stopwatch.StartNew();
             string message;
-            try
+			try
 			{
+				// verifie if the subdirectories exists and create them if necessary
 				if (!Directory.Exists(this.dirTarget)) 
 				{
 					Directory.CreateDirectory(this.dirTarget);
@@ -32,19 +35,48 @@ namespace Core.Model
 					Directory.CreateDirectory(dirPath.Replace(dirSource, dirTarget));
 				}
 
+				// Copie all the files to the target dir
 				foreach (string fileSource in Directory.GetFiles(dirSource, "*.*", SearchOption.AllDirectories))
 				{
-					Stopwatch watch = System.Diagnostics.Stopwatch.StartNew();
-                    string fileTarget = fileSource.Replace(dirSource, dirTarget);
-                    File.Copy(fileSource, fileTarget, true);
-                    watch.Stop();
-                    double elapsedMs = watch.ElapsedMilliseconds;
-                    WriteDailyLog(fileSource, fileTarget, elapsedMs);
-				}
+					Stopwatch watch = Stopwatch.StartNew();
+					string fileTarget = fileSource.Replace(dirSource, dirTarget);
+					string fileExtensionsToEncrypty = AppConfigManager.Instance.GetAppConfigParameter("EncryptionExtensions");
+					String[] LstFileExtensionsToEncrypt = fileExtensionsToEncrypty.Split(",");
+					bool shouldEncrypt = LstFileExtensionsToEncrypt.Any(ext => fileSource.EndsWith(ext.Trim(), StringComparison.OrdinalIgnoreCase));
+
+                    double encryptionTime = 0;
+                    if (shouldEncrypt)
+					{
+						CryptoManager.SetKey("12");
+						Stopwatch EncryptTimer = Stopwatch.StartNew();
+
+						try
+						{
+							CryptoManager.EncryptFile(fileSource);
+						}
+						catch (Exception ex)
+						{
+							encryptionTime = -1;
+						}
+						EncryptTimer.Stop();
+						encryptionTime = EncryptTimer.Elapsed.Milliseconds;
+
+						string newFileName = fileSource + ".xor";
+						fileTarget += ".xor";
+						File.Copy(newFileName, fileTarget, true);
+						File.Delete(newFileName);
+					}
+					else {
+						File.Copy(fileSource, fileTarget, true);
+					}
+					watch.Stop();
+					double elapsedMs = watch.ElapsedMilliseconds;
+                    WriteDailyLog(fileSource, fileTarget, elapsedMs, encryptionTime);
+                }
                 jobTimer.Stop();
-                message = "Job Succeed!";
-                WriteStatusLog(jobTimer.ElapsedMilliseconds, message);
-            }
+				message = "Job Succeed!";
+				WriteStatusLog(jobTimer.ElapsedMilliseconds, message);
+			}
 			catch (Exception ex)
             {
                 jobTimer.Stop();
@@ -54,17 +86,18 @@ namespace Core.Model
             }
         }
 
-        public void WriteDailyLog(string fileSource, string fileTarget, double transfertTime)
+        public void WriteDailyLog(string fileSource, string fileTarget, double transfertTime, double encryptionTime)
         {
             FileInfo fileInfo = new FileInfo(fileSource);
             Dictionary<string, object> logEntry = new Dictionary<string, object>
 			{
-                { "FileName", Name },
+				{ "FileName", Name },
 				{ "SourcePath", fileSource },
-			    { "TargetPath", fileTarget },
-			    { "FileSize", fileInfo.Length },
-			    { "FileTransferTime", transfertTime},
-			    { "TimeStamp", DateTime.Now.ToString("M/d/yyyy HH:mm:ss") }
+				{ "TargetPath", fileTarget },
+				{ "FileSize", fileInfo.Length },
+				{ "FileTransferTime", transfertTime},
+				{ "TimeStamp", DateTime.Now.ToString("M/d/yyyy HH:mm:ss") },
+				{"encryptionTime", encryptionTime }
 			};
 			_log.AddLogInfo(LogType.Daily, logEntry);
 		}
